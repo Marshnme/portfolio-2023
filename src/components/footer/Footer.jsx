39c3620de9ca5d018b815e7c90eaadca1e0a6464
Joshua Holtsclaw <jildsmiles@yahoo.com>
2023-03-28 13:46:45 -0400

    basic footer component created

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.css';
+
+const Footer = () => {
+	return (
+		<div className={styles.footerSection}>
+			<p>Created by Joshua Holtsclaw - 2023</p>
+		</div>
+	);
+};
+
+export default Footer;
